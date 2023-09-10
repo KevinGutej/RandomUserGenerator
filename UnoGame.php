886fa966c7ec9683645053e1d0bb317a6d9383fc
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-09-10 21:50:53 +0200

    Adding function play

--- UnoGame.php
+++ UnoGame.php
@@ -22,4 +22,16 @@ class UnoGame {
     private function shuffleDeck() {
         shuffle($this->deck);
     }
+
+    public function play() {
+        $currentPlayerIndex = 0;
+
+    while (true) {
+        $currentPlayer = $this->players[$currentPlayerIndex];
+        echo "Current player: {$currentPlayer->getName()}\n";
+        echo "Top card in discard pile: {$this->discardPile[count($this->discardPile) - 1]}\n";
+        echo "Your hand: " . implode(", ", $currentPlayer->getHand()) . "\n";
+
+        $playableCards = $this->getPlayableCards($currentPlayer);
+    }
 }
